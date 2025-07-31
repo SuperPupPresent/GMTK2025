947a5fb06d4beb18e2779d7f158ecdd686bf7683
KomodoKid <vishnurprasad1043@gmail.com>
2025-07-30 21:49:52 -0400

    some stuff

--- Assets/Scripts/PlayerAttack.cs
+++ Assets/Scripts/PlayerAttack.cs
@@ -1,9 +1,12 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class PlayerAttack : MonoBehaviour
 {
     public InputActionAsset playerInputActions;
+    public GameObject lightHitbox; // Reference to the light attack hitbox
+    public float lightDuration = 0.5f; // Duration for the light attack hitbox
 
     private InputAction lightAttack;
     private InputAction heavyAttack;
@@ -14,6 +17,7 @@ public class PlayerAttack : MonoBehaviour
     private void OnEnable()
     {
         playerInputActions.FindActionMap("PlayerAttack").Enable();
+        Debug.Log("PlayerAttack action map enabled");
     }
 
     private void OnDisable()
@@ -23,22 +27,67 @@ public class PlayerAttack : MonoBehaviour
 
     private void Awake()
     {
-        lightAttack = InputSystem.actions.FindAction("light");
-        heavyAttack = InputSystem.actions.FindAction("heavy");
-        specialAttack = InputSystem.actions.FindAction("special");
-        grab = InputSystem.actions.FindAction("grab");
-        block = InputSystem.actions.FindAction("block");
+        Debug.Log("PlayerAttack script awake");
+        playerInputActions.FindActionMap("PlayerAttack").Enable();
+
+        lightAttack = InputSystem.actions.FindAction("light"); // Z or West Button
+        heavyAttack = InputSystem.actions.FindAction("heavy"); // X or North Button
+        specialAttack = InputSystem.actions.FindAction("special"); // C or East Button
+        grab = InputSystem.actions.FindAction("grab"); // A or Shoulder Button
+        block = InputSystem.actions.FindAction("block"); // S or Trigger Button
+
     }
+
+
+    private IEnumerator PerformLightAttack() 
+    { 
+        lightHitbox.SetActive(true); // Activate the hitbox
+        yield return new WaitForSeconds(lightDuration); // Wait for the duration of the light attack
+        lightHitbox.SetActive(false); // Deactivate the hitbox
+    }
+
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        Debug.Log("PlayerAttack script started");
+        lightHitbox.SetActive(false); // Ensure the hitbox is inactive at start
+       
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //if the light attack input is triggered, perform the light attack
+        if (lightAttack.WasPressedThisFrame())
+        {
+            Debug.Log("Light Attack Triggered");
+            StartCoroutine(PerformLightAttack());
+        }
+        // Check for heavy attack input
+        if (heavyAttack.WasPressedThisFrame())
+        {
+            Debug.Log("Heavy Attack Triggered");
+            // Implement heavy attack logic here
+        }
+        // Check for special attack input
+        if (specialAttack.WasPressedThisFrame())
+        {
+            Debug.Log("Special Attack Triggered");
+            // Implement special attack logic here
+        }
+        // Check for grab input
+        if (grab.WasPressedThisFrame())
+        {
+            Debug.Log("Grab Triggered");
+            // Implement grab logic here
+        }
+        // Check for block input
+        if (block.WasPressedThisFrame())
+        {
+            Debug.Log("Block Triggered");
+            // Implement block logic here
+        }
+
     }
 }
