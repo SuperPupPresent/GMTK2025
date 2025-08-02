5336ea45b430189a9b24b09f8501b9f83a332eb9
SuperPupPresent <antfleck07@gmail.com>
2025-08-02 00:17:48 -0400

    Added the comic opening

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -1,16 +1,24 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
 
 public class Menu : MonoBehaviour
 {
+
+    InputAction jumpButton;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        jumpButton = InputSystem.actions.FindAction("jump");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (jumpButton.WasPressedThisFrame())
+        {
+            SceneManager.LoadScene("Game");
+        }
     }
 }
